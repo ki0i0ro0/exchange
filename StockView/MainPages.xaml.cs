420e4a80ab6e9090adea38b8fdbfeee10202067a
K.M <ki0i0ro0@gmail.com>
2020-05-15 20:10:28 +0900

    Test
    KKK

---
+++ StockView/MainPages.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace StockView
+{
+    public partial class MainPages : ContentPage
+    {
+        public MainPages()
+        {
+            InitializeComponent();
+        }
+    }
+}
